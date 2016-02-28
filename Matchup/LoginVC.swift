f26b181b0b72aa20e52f39f6d440fcbe927d2ca4
Randall Weylin Reynolds <randall.w.reynolds@comcast.net>
2016-02-28 18:35:29 -0500

    First working version of generic iOS app. Contains login screen, signup screen and homepage. Only the transfer from login to signup screen currently works.

---
+++ Matchup/LoginVC.swift
@@ -0,0 +1,31 @@
+//
+//  LoginVC.swift
+//  Matchup
+//
+//  Created by Randall Reynolds on 2/27/16.
+//  Copyright © 2016 Randall Andrew Sam Sam. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func signinTapped(sender: UIButton) {
+        // Authentication code
+        
+    }
+    
+
+}
