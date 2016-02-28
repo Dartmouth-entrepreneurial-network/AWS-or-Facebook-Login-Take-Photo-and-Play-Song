871970bf3a14750581b915156b41564c3d8fd364
Randall Weylin Reynolds <randall.w.reynolds@comcast.net>
2016-02-27 19:04:08 -0500

    Initial Commit

---
+++ Matchup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Matchup
+//
+//  Created by Randall Reynolds on 2/27/16.
+//  Copyright © 2016 Randall Andrew Sam Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
