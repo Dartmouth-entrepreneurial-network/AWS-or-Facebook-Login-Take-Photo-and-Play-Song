e3b85baba06d4000f732803c71f4268ce8b9950e
rwreynol <randall.weylin.reynolds@gmail.com>
2016-03-02 20:50:44 -0500

    Began installing basis for talking to MySQL server.

--- Matchup/SignupVC.swift
+++ Matchup/SignupVC.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SignupVC: UIViewController {
     
+    
+    /* https://github.com/rwreynol/mysql_server */
+    
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var txtConfirmPassword: UITextField!
@@ -25,9 +28,6 @@ class SignupVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-        
-        
     @IBAction func registerButtonTapped(sender: AnyObject) {
         let userEmail = txtEmail.text;
         let userPassword = txtPassword.text;
@@ -52,11 +52,82 @@ class SignupVC: UIViewController {
             return;
         }
         
-        // Store data
+        // Send data to the server site
+        let myUrl = NSURL(string: "https://github.com/rwreynol/mysql_server/userRegistration.php");
+        //let myUrl = NSURL(string: "http://www.swiftdeveloperblog.com/http-post-example-script/");
+        //let myUrl = NSURL(string: "/Users/randallreynolds/userRegistation.php");
+        
+        //"http://www.swiftdeveloperblog.com/http-post-example-script/"
+        let request = NSMutableURLRequest(URL:myUrl!);
+        request.HTTPMethod="POST";
+        
+        let postString = "email=\(userEmail)&password=\(userPassword)";
+        //let postString = "firstName=James&lastName=Bond";
+        print(postString);
+        
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        
+        // There is a video that goes through the below code that I should look at.
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request){
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            print("****** response =\(response)");
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+                
+                if let parseJSON = json {
+                    
+                    /*
+                    let firstNameValue = parseJSON["firstName"] as? String
+                    print("firstNameValue: \(firstNameValue)");
+                    */
+                    
+                    let resultValue = parseJSON["status"] as? String
+                    print("result: \(resultValue)")
+                    
+                    var isUserRegistered:Bool = false;
+                    if (resultValue=="Success") { isUserRegistered = true; }
+                    
+                    var messageToDisplay:String = parseJSON["message"] as! String;
+                    if (isUserRegistered)
+                    {
+                        messageToDisplay = parseJSON["message"] as! String;
+                    }
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        
+                        // Display alert message with confirmation
+                        let myAlert = UIAlertController(title:"Alert",message:messageToDisplay,preferredStyle: UIAlertControllerStyle.Alert);
+                        
+                        let okAction = UIAlertAction(title:"Ok",style: UIAlertActionStyle.Default){action in
+                            self.dismissViewControllerAnimated(true, completion: nil);
+                        }
+                        myAlert.addAction(okAction);
+                        self.presentViewController(myAlert, animated: true, completion: nil);
+                    });
+                    
+                }
+            } catch let error as NSError {
+                print("**** ERROR ****")
+                print(error);
+                print("**** ERROR ****")
+            }
+        }
+        
+        task.resume();
+        
+        
+        /*
+        
         NSUserDefaults.standardUserDefaults().setObject(userEmail,forKey:"userEmail");
         NSUserDefaults.standardUserDefaults().setObject(userPassword,forKey:"userPassword");
         NSUserDefaults.standardUserDefaults().synchronize();
-        
         
         // Display alert message with confirmation
         let myAlert = UIAlertController(title:"Alert",message:"Sign up successful",preferredStyle: UIAlertControllerStyle.Alert);
@@ -66,6 +137,8 @@ class SignupVC: UIViewController {
         
         myAlert.addAction(okayAction);
         self.presentViewController(myAlert,animated:true,completion:nil);
+
+        */
 
     }
     
