f5cc8478f1dbef706dcff29ca1fb352eb4c0b34e
Sam Putnam <samhputnam@gmail.com>
2016-04-13 21:47:57 -0400

    Added button with AlertView

--- generic/ViewController.swift
+++ generic/ViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-    
+    var helloButton: UIButton = UIButton (frame: CGRect(x: 0, y: 0, width: 100, height: 25))
+    var imageFromSource = UIImagePickerController()
     @IBOutlet var imageView : UIImageView!
     @IBAction func captureImage(){
+    imageFromSource.delegate = self
+    imageFromSource.allowsEditing = false
     
-        var imageFromSource = UIImagePickerController()
-        imageFromSource.delegate = self
-        imageFromSource.allowsEditing = false
+  
+      
         
         
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
@@ -38,40 +40,28 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imageView.image = temp
         self.dismissViewControllerAnimated(true, completion: {})
     }
+    
+    func showMessage(){
+        var msgTitle : String = "Hello World"
+        var messageString : String = "Thanks for the visit!"
+        
+        var alert : UIAlertView = UIAlertView()
+        alert.title = msgTitle
+        alert.message = messageString
+        alert.addButtonWithTitle("OK")
+        alert.show()
+    }
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var loginButton: FBSDKLoginButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //self.loginButton.delegate = self
-        /*
-        if (FBSDKAccessToken.currentAccessToken() != nil)
-        {
-            // User is already logged in, do work such as go to next view controller.
-        }
-        else
-        {
-            let loginButton : FBSDKLoginButton = FBSDKLoginButton()
-            self.view.addSubview(loginButton)
-            loginButton.center = self.view.center
-            loginButton.readPermissions = ["public_profile", "email", "user_friends"]
-            loginButton.delegate = self
-        }*/
-        
+        helloButton.setTitle("A button", forState:UIControlState.Normal)
+        helloButton.backgroundColor = UIColor.blueColor()
+        helloButton.addTarget(self, action: "showMessage", forControlEvents: UIControlEvents.TouchUpInside)
+        helloButton.center = CGPointMake(160, 300)
+        view.addSubview(helloButton)
     }
-    /*func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        if ((error) != nil) {
-            // Process error
-        }
-        else if result.isCancelled {
-            // Handle cancellations
-        }
-        else {
-            // Navigate to other view
-        }
-    }*/
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -92,10 +82,5 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         NSUserDefaults.standardUserDefaults().setBool(false,forKey:"isUserLoggedIn");
         NSUserDefaults.standardUserDefaults().synchronize();
         self.performSegueWithIdentifier("goto_login", sender: self)
-    }
-    /*
-    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
-        // Something to be written later
-    }*/
 }
-
+}
